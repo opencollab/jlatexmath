d38252539efccbb23ea54a7a0df82130ecaff5e4
Calixte DENIZET <calixte.denizet@ac-rennes.fr>
2010-06-30 07:33:19 +0200

    Remove an unused method

--- src/org/scilab/forge/jlatexmath/ArrayOfAtoms.java
+++ src/org/scilab/forge/jlatexmath/ArrayOfAtoms.java
@@ -44,10 +44,6 @@ public class ArrayOfAtoms extends TeXFormula {
 	row = 0;
     }
     
-    public Atom getLastAtom() {
-	return array.peekLast().pollLast();
-    }
-
     public void addCol() {
 	array.get(row).add(root);
 	root = null;
