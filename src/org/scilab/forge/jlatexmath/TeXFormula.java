ac4dce561c0b553cc8c80bab5e00eb03aac6f197
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2013-02-25 14:53:18 +0100

    Prepare 1.0.1 release

--- src/org/scilab/forge/jlatexmath/TeXFormula.java
+++ src/org/scilab/forge/jlatexmath/TeXFormula.java
@@ -81,7 +81,7 @@ import java.lang.Character.UnicodeBlock;
  */
 public class TeXFormula {
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     public static final int SERIF = 0;
     public static final int SANSSERIF = 1;
