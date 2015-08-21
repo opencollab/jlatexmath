330034a31044ebf4c6854db581cc45ce93c09285
Calixte DENIZET <calixte.denizet@scilab-enterprises.com>
2015-08-21 13:55:24 +0200

    Fix color displayed with the command bgcolor

--- src/org/scilab/forge/jlatexmath/Box.java
+++ src/org/scilab/forge/jlatexmath/Box.java
@@ -272,7 +272,7 @@ public abstract class Box {
         prevColor = g2.getColor();
         if (background != null) { // draw background
             g2.setColor(background);
-            //g2.fill(new Rectangle2D.Float(x, y - height, width, height + depth));
+            g2.fill(new Rectangle2D.Float(x, y - height, width, height + depth));
         }
         if (foreground == null) {
             g2.setColor(prevColor); // old foreground color
