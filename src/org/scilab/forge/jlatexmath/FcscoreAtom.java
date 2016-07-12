759faf9a8a647f25ec8c8dbcaa6f6acefb94af61
calixteman <calixte.denizet@gmail.com>
2016-07-12 15:43:19 +0200

    Reindent the code

--- src/org/scilab/forge/jlatexmath/FcscoreAtom.java
+++ src/org/scilab/forge/jlatexmath/FcscoreAtom.java
@@ -24,23 +24,23 @@
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
  *
- * Linking this library statically or dynamically with other modules 
- * is making a combined work based on this library. Thus, the terms 
- * and conditions of the GNU General Public License cover the whole 
+ * Linking this library statically or dynamically with other modules
+ * is making a combined work based on this library. Thus, the terms
+ * and conditions of the GNU General Public License cover the whole
  * combination.
- * 
- * As a special exception, the copyright holders of this library give you 
- * permission to link this library with independent modules to produce 
- * an executable, regardless of the license terms of these independent 
- * modules, and to copy and distribute the resulting executable under terms 
- * of your choice, provided that you also meet, for each linked independent 
- * module, the terms and conditions of the license of that module. 
- * An independent module is a module which is not derived from or based 
- * on this library. If you modify this library, you may extend this exception 
- * to your version of the library, but you are not obliged to do so. 
- * If you do not wish to do so, delete this exception statement from your 
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce
+ * an executable, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting executable under terms
+ * of your choice, provided that you also meet, for each linked independent
+ * module, the terms and conditions of the license of that module.
+ * An independent module is a module which is not derived from or based
+ * on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obliged to do so.
+ * If you do not wish to do so, delete this exception statement from your
  * version.
- * 
+ *
  */
 
 package org.scilab.forge.jlatexmath;
@@ -53,7 +53,7 @@ public class FcscoreAtom extends Atom {
     private int N;
 
     public FcscoreAtom(int N) {
-	this.N = N;
+        this.N = N;
     }
 
     public int getLeftType() {
@@ -63,10 +63,10 @@ public class FcscoreAtom extends Atom {
     public int getRightType() {
         return TeXConstants.TYPE_ORDINARY;
     }
-    
-    public Box createBox(TeXEnvironment env) {
-	final float factor = 12 * SpaceAtom.getFactor(TeXConstants.UNIT_MU, env);
 
-	return new FcscoreBox(N == 5 ? 4 : N, factor * 1f, factor * 0.07f, factor * 0.125f, N == 5);
-    } 
+    public Box createBox(TeXEnvironment env) {
+        final float factor = 12 * SpaceAtom.getFactor(TeXConstants.UNIT_MU, env);
+
+        return new FcscoreBox(N == 5 ? 4 : N, factor * 1f, factor * 0.07f, factor * 0.125f, N == 5);
+    }
 }
